c45402e1a4b575197e2bb1d8dc3f4eb1cc2222cd
Pradyumn <pradyumnkrshukla.4672@gmail.com>
2026-03-07 22:31:22 +0530

    feat: Implement a number guessing game with best score persistence.

--- CodeWithHarryPython/MiniProject-PerfectGuess/main.py
+++ CodeWithHarryPython/MiniProject-PerfectGuess/main.py
@@ -1,4 +1,22 @@
 import random
 
-number = random.randint(1,100)
-print(number) 
+WinningNumber = random.randint(1,100)
+
+Guesses = 0
+
+while True:
+    Guesses += 1
+    UserGuess = int(input("Enter Your Guess : "))
+    if UserGuess == WinningNumber:
+        print("You Win!")
+        print(f"You guessed the number in {Guesses} guesses")
+        with open("score.txt", "r") as f:
+            score = int(f.read())
+        if score == 0 or Guesses < score:
+            with open("score.txt", "w") as f:
+                f.write(str(Guesses))
+        break
+    elif UserGuess < WinningNumber:
+        print("Too Low")
+    else:
+        print("Too High")
